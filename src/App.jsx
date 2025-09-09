6af0770924be5b80df496b817a25c6bc1bb42c97
wafi1122 <fstimatr00040@gmail/com>
2025-09-09 15:04:19 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <div className='logo-box'>
+          <img src="/twitter.png" alt="" className='twitter-logo'/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
